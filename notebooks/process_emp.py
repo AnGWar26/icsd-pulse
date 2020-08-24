49aa62eb7f3b72b2b6657fbd06e2dd25b3794b0a
AnGWar26 <andrew.warfield@protonmail.com>
2020-08-24 15:52:05 -0700

    emp: switch around logic

--- notebooks/process_emp.py
+++ notebooks/process_emp.py
@@ -9,16 +9,38 @@ def process_emp(week,sheet_name=0):
     
     """
     
-    if int(week) not in range(1,7):
-        emp = pd.read_excel('../data/emp/employ1_week' + str(week) + '.xlsx', sheet_name=sheet_name, skip_rows=[0,1,2], header=[3,4], index_col=0, skipfooter=1, nafilter=False, keep_default_na=False)
+    if int(week) in range(1,7):
+        emp = pd.read_excel('../data/emp/employ1_week' + str(week) + '.xlsx', sheet_name=sheet_name, skip_rows=[0,1,2],
+                            header=[3,4], index_col=0, nafilter=False, keep_default_na=False)
+
     else:
-        emp = pd.read_excel('../data/emp/employ1_week' + str(week) + '.xlsx', sheet_name=sheet_name, skip_rows=[0,1,2], header=[3,4], index_col=0, nafilter=False, keep_default_na=False)
+        emp = pd.read_excel('../data/emp/employ1_week' + str(week) + '.xlsx', sheet_name=sheet_name, skip_rows=[0,1,2],
+                            header=[3,4], index_col=0, skipfooter=1, nafilter=False, keep_default_na=False)
 
     emp.rename(columns={'Unnamed: 1_level_1':'Total'},level=1,inplace=True)
     #whitespace in the index names from the spreadsheet make things hard to work with
     emp.index = emp.index.str.strip()
     
-    if int(week) not in range(1,7):
+    if int(week) in range(1,7):
+        emp.drop(['Age','Sex','Hispanic origin and Race','Education', 'Marital status' ,
+                  'Presence of children under 18 years old', 'Health status','Household income'
+         ], axis=0,inplace=True)
+        emp.index = ['Total', '18 - 24', '25 - 39', '40 - 54', '55 - 64', '65 and above',
+        'Male', 'Female', 'Hispanic or Latino (may be of any race)',
+       'White alone, not Hispanic', 'Black alone, not Hispanic',
+       'Asian alone, not Hispanic',
+       'Two or more races + Other races, not Hispanic',
+       'Less than high school', 'High school or GED',
+       'Some college/associate’s degree', 'Bachelor’s degree or higher',
+       'Married', 'Widowed', 'Divorced/separated', 'Never married',
+       'Did not report marriage status',
+       'Children in household', 'No children', 'Excellent', 'Very good',
+       'Good', 'Fair', 'Poor', 'Did not report health status',
+       'Less than \\$25,000', '\\$25,000 - \\$34,999',
+       '\\$35,000 - \\$49,999', '\\$50,000 - \\$74,999', '\\$75,000 - \\$99,999',
+       '\\$100,000 - \\$149,999', '\\$150,000 - \\$199,999', '\\$200,000 and above',
+        'Did not report income']
+    else:
         emp.drop(['Age','Sex','Hispanic origin and Race','Education', 'Marital status' ,
                   'Presence of children under 18 years old', 'Health status','Household income',
                   'Used in the last 7 days to meet spending needs*', 'Household size'
@@ -45,25 +67,6 @@ def process_emp(week,sheet_name=0):
        'Borrowing from friends or family', 'Unemployment insurance (UI) benefit payments',
        'Stimulus (economic impact) payment', 'Money saved from deferred or forgiven payments (to meet spending needs)',
        'Did not report']
-    else:
-        emp.drop(['Age','Sex','Hispanic origin and Race','Education', 'Marital status' ,
-                  'Presence of children under 18 years old', 'Health status','Household income'
-         ], axis=0,inplace=True)
-        emp.index = ['Total', '18 - 24', '25 - 39', '40 - 54', '55 - 64', '65 and above',
-        'Male', 'Female', 'Hispanic or Latino (may be of any race)',
-       'White alone, not Hispanic', 'Black alone, not Hispanic',
-       'Asian alone, not Hispanic',
-       'Two or more races + Other races, not Hispanic',
-       'Less than high school', 'High school or GED',
-       'Some college/associate’s degree', 'Bachelor’s degree or higher',
-       'Married', 'Widowed', 'Divorced/separated', 'Never married',
-       'Did not report marriage status',
-       'Children in household', 'No children', 'Excellent', 'Very good',
-       'Good', 'Fair', 'Poor', 'Did not report health status',
-       'Less than \\$25,000', '\\$25,000 - \\$34,999',
-       '\\$35,000 - \\$49,999', '\\$50,000 - \\$74,999', '\\$75,000 - \\$99,999',
-       '\\$100,000 - \\$149,999', '\\$150,000 - \\$199,999', '\\$200,000 and above',
-        'Did not report income']
     # Applies to all dataframes
     emp.replace('-', 0,inplace=True) # replace values that are not ints
     #init the columns or else it errors
